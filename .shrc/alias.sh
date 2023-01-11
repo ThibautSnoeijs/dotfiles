c8723a448997af00c4412b8ddeaf55227a8e5ef3
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2023-01-11 18:43:07 +0100

    Update alias.sh

--- .shrc/alias.sh
+++ .shrc/alias.sh
@@ -1,17 +1,20 @@
+#!/bin/bash
 IFS=$'\n'
 
-defaultsdir=~/.shrc/aliases/defaults/
+aliasesdir=$shrcdir/aliases
+
+# This is for commands that you want to have the same flags by default
+# Example: "ls" is in "--color=auto" so an alias will be "alias ls=ls --color=auto"
+defaultsdir=$aliasesdir/defaults
 [[ -d "$defaultsdir" ]]&& for a in $(ls -1 "$defaultsdir");do
-	for L in $(sed "s/\s*#.*//g; /^$/ d" "$defaultsdir$a");do 
+	for L in $(sed "s/\s*#.*//g; /^$/ d" "$defaultsdir/$a");do 
 		alias $L="$L $a"
 	done
 done
 
-other=~/.shrc/aliases/other
-[[ -f "$other" ]]&& for L in $(sed "s/\s*#.*//g; /^$/ d" "$other");do 
-	alias $(echo $L |cut -d'=' -f 1 )="$(echo $L |cut -d'=' -f 2- )"
-done
-
 unset IFS
-unset other
 unset defaultsdir
+unset aliasesdir
+
+# Put other aliases here
+alias boottime='cowsay $( echo boottime is && sudo dmesg | grep "New seat" | cut -d"]" -f 1 | rev | cut -d" " -f 1 | rev && echo seconds ) | lolcat'
