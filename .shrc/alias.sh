0f9e6a68ab35411f61c71f80b6c9232a83c8e308
Thibaut <108188070+ThibautSnoeijs@users.noreply.github.com>
2024-02-06 18:09:55 +0100

    Update alias.sh

--- .shrc/alias.sh
+++ .shrc/alias.sh
@@ -4,16 +4,26 @@ IFS=$'\n'
 aliasesdir=$shrcdir/aliases
 
 # This is for commands that you want to have the same flags by default
-# Example: "ls" is in "--color=auto" so an alias will be "alias ls=ls --color=auto"
+# Example: "ls" is in "--color=auto" so an alias will be "ls=ls --color=auto"
 defaultsdir=$aliasesdir/defaults
 [[ -d "$defaultsdir" ]]&& for a in $(ls -1 "$defaultsdir");do
 	for L in $(sed "s/\s*#.*//g; /^$/ d" "$defaultsdir/$a");do 
 		alias $L="$L $a"
 	done
 done
+unset defaultsdir
+
+# This is for commands that you want to have run by other programs such as doas or sudo
+# Example: "reboot" is in "sudo" so an alias will be "reboot=sudo reboot"
+dodir=$aliasesdir/do
+[[ -d "$dodir" ]]&& for a in $(ls -1 "$dodir");do
+	for L in $(sed "s/\s*#.*//g; /^$/ d" "$dodir/$a");do
+		alias $L="$a $L"
+	done
+done
+unset dodir
 
 unset IFS
-unset defaultsdir
 unset aliasesdir
 
 # Put other aliases here
